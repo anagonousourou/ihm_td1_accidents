bf54e5fbdea3a8bcad7938b779d1c244cec387d6
ANAGONOU Patrick <50173133+anagonousourou@users.noreply.github.com>
2020-04-08 16:04:58 +0200

    Update build-server.js

--- app/build-server.js
+++ app/build-server.js
@@ -12,5 +12,5 @@ module.exports = (cb) => {
   app.use(morgan('[:date[iso]] :method :url :status :response-time ms - :res[content-length]'))
   app.use('/api', api)
   app.use('*', (req, res) => res.status(404).end())
-  const server = app.listen(process.env.PORT || 9428, () => cb && cb(server))
+  const server = app.listen(process.env.PORT || 5000, () => cb && cb(server))
 }
