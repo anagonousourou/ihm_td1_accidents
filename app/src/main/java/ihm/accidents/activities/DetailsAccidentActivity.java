3719be0e2df4cd981de931908d715b507dadcae3
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2020-04-09 15:15:34 +0200

    databindind failed try from start

--- app/src/main/java/ihm/accidents/activities/DetailsAccidentActivity.java
+++ app/src/main/java/ihm/accidents/activities/DetailsAccidentActivity.java
@@ -8,7 +8,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 
+import ihm.accidents.databinding.DetailsAccidentActivityBinding;
 import ihm.accidents.models.AccidentModel;
 import ihm.accidents.utils.Utils;
 import ihm.accidents.R;
@@ -20,14 +22,16 @@ import ihm.accidents.R;
  */
 public class DetailsAccidentActivity extends Activity {
     private static final String TAG = "DetailsAccidentActivity";
+    DetailsAccidentActivityBinding dataBinding;
     private AccidentModel accidentModel;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.details_accident_activity);
-
+        dataBinding= DataBindingUtil.setContentView(this,R.layout.details_accident_activity);
         Intent intent = getIntent();
         this.accidentModel = intent.getParcelableExtra(Utils.accidentKey);
+        dataBinding.setAccidentData(this.accidentModel);
+        /*
         TextView titretv =  this.findViewById(R.id.titre_details_accident_txtview);
         TextView lieutv =  this.findViewById(R.id.lieu_accident_txtview);
         TextView momenttv = this.findViewById(R.id.temps_accident_txtview);
@@ -40,7 +44,7 @@ public class DetailsAccidentActivity extends Activity {
 
         momenttv.setText(getString(R.string.temps_accident,accidentModel.userFormatDate()));
         lieutv.setText(getString(R.string.lieu_accident, accidentModel.getAddress()));
-        titretv.setText(getString(R.string.titre_accident, accidentModel.getTitle()));
+        titretv.setText(getString(R.string.titre_accident, accidentModel.getTitle()));*/
 
     }
     public void goToSendMessage(View view) {
