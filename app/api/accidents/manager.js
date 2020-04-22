ba90d93b1d6a8ff42359c5fe0e859a76bd51306a
Florian_Striebel <florian.striebel@live.fr>
2020-04-22 16:19:58 +0200

    modifiacation algo de distance

--- app/api/accidents/manager.js
+++ app/api/accidents/manager.js
@@ -2,19 +2,40 @@ const { Accident } = require('../../models')
 
 const buildListNear = (latitude,longitude,distance) => {
     const accidents = Accident.get();
-    return accidents.filter((accident)=>
-        isNear(latitude,longitude,distance,accident.id))
+    return accidents.filter((accident)=>{
+        console.log(accident);
+        return isNear(latitude,longitude,distance,accident.id);
+})
 }
 
 const isNear = (latitude,longitude,distance,accidentId) => {
     const accident= Accident.getById(accidentId);
-    const distanceToAcc = 6371 * Math.acos((Math.sin(latitude)*Math.sin(accident.latitude))+(Math.cos(latitude)*Math.cos(accident.latitude)*Math.cos(longitude-accident.longitude)))
-    //console.log("distance to acc "+distanceToAcc)
-   // console.log("distance "+distance)
+
+    const diffLat= toRad(accident.latitude-latitude);
+    const diffLong= toRad(accident.longitude-longitude);
+    
+    const latAcc = toRad(accident.latitude);
+    const latUser = toRad(latitude);
+
+    const a = (Math.sin(diffLat/2) * Math.sin(diffLat/2))+
+    (Math.sin(diffLong/2) * Math.sin(diffLong/2) * 
+    Math.cos(latAcc) * Math.cos(latUser));
+    
+    const b = 2* Math.atan(Math.sqrt(a)/Math.sqrt(1-a));
+
+    const distanceToAcc = 6371 * b
+    //console.log("latitude"+accident.latitude)
+    //console.log("longitude"+accident.longitude)
+    console.log("distance to acc "+distanceToAcc)
+    //console.log("distance "+distance)
     //console.log(distanceToAcc<=distance)
     return distanceToAcc<=distance;
 }
 
+function toRad(Value) 
+{
+    return Value * Math.PI / 180;
+}
 module.exports = {
     buildListNear,
   }
