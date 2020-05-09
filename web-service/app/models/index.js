3e0ea8304a685c829217c4ac3f912273553ba6d6
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2020-05-10 00:47:54 +0200

    delete webservice folder$

--- web-service/app/models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require('./user.model.js');
-const Accident = require('./accident.model.js');
-module.exports = {
- 
-  User,
-  Accident
-}
--- web-service/app/models/user.model.js
+++
@@ -1,7 +0,0 @@
-const Joi = require('joi')
-const BaseModel = require('../utils/base-model.js')
-
-module.exports = new BaseModel('User', {
-  username: Joi.string().required(),
-  password: Joi.string().required(),
-})
